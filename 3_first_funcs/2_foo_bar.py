73d37ac3478a32311242241df1ee0a9a2bc4f6b3
Wojtowicz <artur.wojtowicz@capgemini.com>
2022-07-28 09:58:08 +0200

    FizzBuzz

--- 3_first_funcs/2_foo_bar.py
+++ 3_first_funcs/2_foo_bar.py
@@ -6,7 +6,22 @@
 # Checking is value divisible in python, You can use modulo f.e. check if value is divisible by 2 ->  x % 2 == 0
 
 
-def foo_bar(n: list) -> list: ...
+def foo_bar(n: list):
+    for num in n:
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
 
+def run(x):
+    if isinstance(x, list):
+        foo_bar(x)
+    else:
+        print("Ooopsie")
 
-foo_bar(list(range(101)))
+run(list(range(1, 151)))
+run("Hehe")
